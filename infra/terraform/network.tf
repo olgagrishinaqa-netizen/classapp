a20adc480e4950bd076d0d42f611e6efaf3188f7
Olga <olgagrishinaqa@gmail.com>
2026-09-07 20:20:06 +0300

    infra: apply locked static ip for high pipeline stability

--- infra/terraform/network.tf
+++ infra/terraform/network.tf
@@ -1,12 +1,20 @@
 resource "yandex_vpc_network" "net" {
-  name = "classapp-net"
+  name = "classapp-network"
 }
 
 resource "yandex_vpc_subnet" "subnet" {
   name           = "classapp-subnet"
-  zone           = var.zone
+  zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.net.id
   v4_cidr_blocks = ["10.10.0.0/24"]
+}
+
+# РЕЗЕРВИРУЕМ ПОСТОЯННЫЙ (СТАТИЧЕСКИЙ) IP-АДРЕС В ОБЛАКЕ
+resource "yandex_vpc_address" "static_ip" {
+  name = "classapp-static-ip"
+  external_ipv4_address {
+    zone_id = "ru-central1-a"
+  }
 }
 
 resource "yandex_vpc_security_group" "sg" {
@@ -15,18 +23,23 @@ resource "yandex_vpc_security_group" "sg" {
 
   ingress {
     protocol       = "TCP"
-    port           = 80
+    description    = "SSH"
     v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 22
   }
 
   ingress {
     protocol       = "TCP"
-    port           = 22
+    description    = "HTTP"
     v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 80
   }
 
   egress {
     protocol       = "ANY"
+    description    = "Outbound traffic"
     v4_cidr_blocks = ["0.0.0.0/0"]
+    from_port      = 0
+    to_port        = 65535
   }
 }
--- infra/terraform/vm.tf
+++ infra/terraform/vm.tf
@@ -6,39 +6,38 @@ resource "yandex_compute_instance" "vm" {
   name        = "classapp-vm"
   platform_id = "standard-v3"
 
-  # Экономия 70%: ВМ может быть прервана облаком
   scheduling_policy {
     preemptible = true
   }
 
   resources {
-    cores         = 2 # Минимально для v3 платформы, но мы экономим за счет прерываемости
-    core_fraction = 50 # Загрузка ядра наполовину срезает базовую стоимость CPU
-    memory        = 2 # 2 ГБ RAM хватит для Flask + Postgres со свопом
+    cores         = 2
+    core_fraction = 50
+    memory        = 2
   }
 
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.id
-      size     = 15  # Сжали диск до 15 ГБ HDD
-      type     = "network-hdd" # Самый дешевый тип диска
+      size     = 15
+      type     = "network-hdd"
     }
   }
 
   network_interface {
     subnet_id          = yandex_vpc_subnet.subnet.id
     nat                = true
+    nat_ip_address     = yandex_vpc_address.static_ip.external_ipv4_address[0].address # Привязка статики
     security_group_ids = [yandex_vpc_security_group.sg.id]
   }
 
   metadata = {
     ssh-keys = "ubuntu:${var.vm_public_ssh_key}"
-    # Включаем автоматическое создание SWAP-файла на 2 ГБ для стабильности Postgres
     user-data = <<-CLOUD
       #cloud-config
       swap:
         filename: /swapfile
-        size: 2147483648 # 2 GB SWAP
+        size: 2147483648
         maxsize: 2147483648
       packages:
         - docker.io
