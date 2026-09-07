a1df2c5ed1274fd682179584d0619610c529543e
Olga <olgagrishinaqa@gmail.com>
2026-09-07 16:06:16 +0300

    infra: migration from managed pg to dockerized db on single low-cost vm with swap

--- infra/terraform/vm.tf
+++ infra/terraform/vm.tf
@@ -6,15 +6,22 @@ resource "yandex_compute_instance" "vm" {
   name        = "classapp-vm"
   platform_id = "standard-v3"
 
+  # Экономия 70%: ВМ может быть прервана облаком
+  scheduling_policy {
+    preemptible = true
+  }
+
   resources {
-    cores  = 2
-    memory = 2
+    cores         = 2 # Минимально для v3 платформы, но мы экономим за счет прерываемости
+    core_fraction = 50 # Загрузка ядра наполовину срезает базовую стоимость CPU
+    memory        = 2 # 2 ГБ RAM хватит для Flask + Postgres со свопом
   }
 
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.id
-      size     = 20
+      size     = 15  # Сжали диск до 15 ГБ HDD
+      type     = "network-hdd" # Самый дешевый тип диска
     }
   }
 
@@ -26,11 +33,17 @@ resource "yandex_compute_instance" "vm" {
 
   metadata = {
     ssh-keys = "ubuntu:${var.vm_public_ssh_key}"
+    # Включаем автоматическое создание SWAP-файла на 2 ГБ для стабильности Postgres
     user-data = <<-CLOUD
       #cloud-config
+      swap:
+        filename: /swapfile
+        size: 2147483648 # 2 GB SWAP
+        maxsize: 2147483648
       packages:
         - docker.io
         - docker-compose-plugin
+        - python3-yaml
       runcmd:
         - [ bash, -lc, "usermod -aG docker ubuntu" ]
     CLOUD
